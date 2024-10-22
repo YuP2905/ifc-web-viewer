4783d1778b81eff3abe407ec328c2fa48880bf3e
Yang Yupeng <118242738+YuP2905@users.noreply.github.com>
2024-10-22 15:21:49 +0900

    Update vue.config.js

--- client/vue.config.js
+++ client/vue.config.js
@@ -33,21 +33,4 @@ module.exports = defineConfig({
   
 });
 
-// import { defineConfig } from "@vue/cli-service";
-// import AutoImport from 'unplugin-auto-import/vite'
-// import Components from 'unplugin-vue-components/vite'
-// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-// export default defineConfig({
-//   transpileDependencies: true,
-//   configureWebpack: {
-//     plugins: [
-//       AutoImport({
-//         resolvers: [ElementPlusResolver()],
-//       }),
-//       Components({
-//         resolvers: [ElementPlusResolver()],
-//       })
-//     ]
-//   }
-// })
